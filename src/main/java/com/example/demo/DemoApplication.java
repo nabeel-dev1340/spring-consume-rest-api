d73bc99451475d2b6b6aa32de4cc8a80833a5f2a
syed nabeel <nabeelsharafat@gmail.com>
2023-03-10 15:32:26 +0500

    removed a method

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -25,12 +24,6 @@ public class DemoApplication {
 			log.info(todo.toString());
 		}
 	}
-
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
 	public static List<Todo> getTodos() {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<Todo[]> response = restTemplate.getForEntity("https://jsonplaceholder.typicode.com/todos", Todo[].class);
